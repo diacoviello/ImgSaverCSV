142db4b03974a734d571734a902602695c57736d
Dave <diacovmusic@gmail.com>
2022-03-30 16:59:15 -0400

    everything is functioning, but the exported images are including surrounding brackets and quotes...will be looking into it

--- downloader.py
+++ downloader.py
@@ -5,10 +5,13 @@ import urllib.request
 
 ##--------------------------
 def url_to_jpg(i, data, file_path):
+    
+    df_name = data[[0]]
+    print(df_name)
 
-    filename = 'icon-{}.jpg'.format(i)
+    filename = df_name + '.jpg'.format(i)
     full_path = '{}{}'.format(file_path, filename)
-    urllib.request.urlretrieve(data, full_path)
+    urllib.request.urlretrieve(data[1], full_path)
 
     print('{} has been saved!'.format(filename))
 
@@ -25,12 +28,12 @@ FILE_PATH = '../LegendsDexTrack/images/'
 # Read list of URLS and apply name based on image subject
 data = pd.read_csv(FILE, delimiter=",")
 
-df = pd.DataFrame(data, columns=[['Name','URL']])
-
-
+df = pd.DataFrame(data)
+# df_name = pd.DataFrame(data, columns=['Name'])
+# print(df_name)
 
 # Save images to directory by iterating through entire list
 for i, data in enumerate(data.values):
-    url_to_jpg(i, data[1], FILE_PATH)
+    url_to_jpg(i, data, FILE_PATH)
 
 # print(data.values)
